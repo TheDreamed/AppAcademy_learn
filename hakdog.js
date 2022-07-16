1380317313bd29658511e9479028607adb043e2d
thedream <markspencerjordan@gmail.com>
2022-07-16 22:15:21 +0800

    Added like function

---
+++ hakdog.js
@@ -0,0 +1,11 @@
+// this program will tell us about our cats
+
+let cats = [
+    {name: 'yumi', likes: 'tails'},
+    {name: 'lulu', likes: 'sleep'},
+    {name: 'hakdog', likes: 'dogs'},
+]
+
+for (let i =0; i< cats.length; i++){
+    console.log(`${cats[i].name} likes ${cats[i].likes}`);
+}
